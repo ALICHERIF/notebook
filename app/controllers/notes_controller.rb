622f065e058aa5d290278efa2f6cd987a00e6323
ALICHERIF <adem.mohamed@hotmail.fr>
2024-03-04 17:55:35 +0100

    creating note new pages

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,17 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: [:show]
   def index
 @notes = Note.all
-
   end
+  def show
+  end
+  def new
+    @note = Note.new
+  end
+
+  private
+  def set_note
+    @note = Note.find(params[:id])
+end
+
 end
